6c6208b72e240131b6a3ec73e381006390f0cc91
Andrew Kvalheim <Andrew@Kvalhe.im>
2019-10-27 06:41:42 -0700

    Work around aws/aws-sdk-js#2914.

--- src/typeorm-aurora-data-api-driver.ts
+++ src/typeorm-aurora-data-api-driver.ts
@@ -3,6 +3,32 @@ import createDataApiClient from 'data-api-client'
 import { transformQueryAndParameters } from './transform.utils'
 
 export default class DataApiDriver {
+  // Workaround for aws/aws-sdk-js#2914
+  private static async withRetry<T>(run: () => Promise<T>, ttl: number = 3): Promise<T> {
+    try {
+      return await run()
+    } catch (error) {
+      if (
+        error.code === 'BadRequestException' &&
+        typeof error.retryDelay === 'number' &&
+        typeof error.message === 'string' &&
+        error.message.startsWith('Communications link failure')
+      ) {
+        if (ttl === 0) {
+          console.log('DB cluster is paused. Retry limit reached.')
+          throw error
+        }
+
+        console.log(`DB cluster is paused. Retrying in ${error.retryDelay.toFixed(1)}s…`)
+        await new Promise(resolve => setTimeout(resolve, error.retryDelay * 1000))
+
+        return DataApiDriver.withRetry(run, ttl - 1)
+      }
+
+      throw error
+    }
+  }
+
   private readonly client: any
   private transactionId?: string
 
@@ -33,27 +59,36 @@ export default class DataApiDriver {
 
     this.loggerFn(transformedQueryData.queryString, transformedQueryData.parameters)
 
-    const result = await this.client.query({
-      sql: transformedQueryData.queryString,
-      parameters: transformedQueryData.parameters,
-      transactionId: this.transactionId,
-    })
+    const result = await DataApiDriver.withRetry(
+      () =>
+        this.client.query({
+          sql: transformedQueryData.queryString,
+          parameters: transformedQueryData.parameters,
+          transactionId: this.transactionId,
+        }) as Promise<any>
+    )
 
     return result.records || result
   }
 
   public async startTransaction(): Promise<void> {
-    const { transactionId } = await this.client.beginTransaction()
+    const { transactionId } = await DataApiDriver.withRetry(
+      () => this.client.beginTransaction() as Promise<any>
+    )
     this.transactionId = transactionId
   }
 
   public async commitTransaction(): Promise<void> {
-    await this.client.commitTransaction({ transactionId: this.transactionId })
+    await DataApiDriver.withRetry(
+      () => this.client.commitTransaction({ transactionId: this.transactionId }) as Promise<any>
+    )
     this.transactionId = undefined
   }
 
   public async rollbackTransaction(): Promise<void> {
-    await this.client.rollbackTransaction({ transactionId: this.transactionId })
+    await DataApiDriver.withRetry(
+      () => this.client.rollbackTransaction({ transactionId: this.transactionId }) as Promise<any>
+    )
     this.transactionId = undefined
   }
 }
